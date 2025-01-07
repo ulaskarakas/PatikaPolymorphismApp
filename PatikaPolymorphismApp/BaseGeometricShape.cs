6e67c145e6071df074f72ca9be96070eb437d57b
ulaskarakas <ulaskarakas95@gmail.com>
2025-01-07 10:06:37 +0300

    Add project files.

---
+++ PatikaPolymorphismApp/BaseGeometricShape.cs
@@ -0,0 +1,62 @@
+﻿namespace PatikaPolymorphismApp
+{
+    public class BaseGeometricShape
+    {
+        // Properties
+        public int Width {  get; set; }
+        public int Height { get; set; }
+
+        // Methods
+        public virtual void CalculateArea()
+        {
+            Console.WriteLine($"Area calculated");
+        }
+    }
+
+    public class Square : BaseGeometricShape
+    {
+        // Constructor
+        public Square(int width) 
+        {
+            Width = width;
+        }
+
+        // Methods
+        public override void CalculateArea() 
+        {
+            Console.WriteLine($"Area: {Width*Width}");
+        }
+    }
+
+    public class Rectangle : BaseGeometricShape 
+    {
+        // Constructor
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        // Methods
+        public override void CalculateArea()
+        {
+            Console.WriteLine($"Area: {Width*Height}");
+        }
+    }
+
+    public class RightTriangle : BaseGeometricShape
+    {
+        // Constructor
+        public RightTriangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        // Methods
+        public override void CalculateArea()
+        {
+            Console.WriteLine($"Area: {(Width * Height)/2}");
+        }
+    }
+}
---
+++ PatikaPolymorphismApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using PatikaPolymorphismApp;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        BaseGeometricShape shapeSquare = new Square(5);
+        BaseGeometricShape shapeRectangle = new Rectangle(5, 3);
+        BaseGeometricShape shapeRightTriangle = new RightTriangle(5, 12);
+
+        shapeSquare.CalculateArea();
+        shapeRectangle.CalculateArea();
+        shapeRightTriangle.CalculateArea();
+    }
+}
